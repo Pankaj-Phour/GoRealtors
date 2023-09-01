a44b26f06dc1e65e878d7f3bb672f6e2f9eee97d
Pankaj Phour <pankaj.phour70@gmail.com>
2023-09-01 16:45:07 +0530

    Projects component added and navbar UI changing for mobile device

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  closeNav(link:any){
+    console.log(link);
+    if(link !== ''){
+      this.router.navigate([link])
+    }
+    
   }
 
 }
