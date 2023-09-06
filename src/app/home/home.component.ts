9d9a93c50b13b48d05d15787bde35bb7c610cf39
Pankaj Phour <pankaj.phour70@gmail.com>
2023-09-06 16:11:32 +0530

    UI updated according to new figma

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+works:any = [
+  {image:'assets/locationMarker.png'},
+  {image:'assets/payment.png'},
+  {image:'assets/secure.png'},
+];
+featured:any = [
+  {image:'assets/locationMarker.png'},
+  {image:'assets/payment.png'},
+  {image:'assets/secure.png'},
+]
   constructor() { }
 
   ngOnInit(): void {
