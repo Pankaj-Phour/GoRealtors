f3a189b1341d93a4167db32b15d7185f59c2094d
Pankaj Phour <95477824+Pankaj-Phour@users.noreply.github.com>
2025-02-09 15:49:45 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
 
-Url : 'https://gorealtors.onrender.com'
+// Url : 'https://gorealtors.onrender.com'
+Url : 'https://gorealtors-grfc.onrender.com'
 
 };
